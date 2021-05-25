d41844518cf53c67099b9121fad5b27badb64f3d
Fabian Daniel <kiloboggaming@gmail.com>
2021-05-25 22:10:24 +0200

    Login-Register page

---
+++ client/login-register/login-register.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="login-register.css">
+  </head>
+
+  <body>
+    <div class="row">
+      <div class="col-md-6 mx-auto p-0">
+          <div class="card">
+              <div class="login-box" id="login-box">
+                  <div class="login-snip"> <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Login</label> <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+                      <div class="login-space">
+                          <form class="login" method="POST" action="">
+                              <div class="group"> <label for="email-login" class="label">Email Address</label> <input id="email-login" name="email" type="email" class="input" placeholder="Enter your email address" required> </div>
+                              <div class="group"> <label for="pass-login" class="label">Password</label> <input id="pass-login" name="password" type="password" class="input" data-type="password" placeholder="Enter your password" required> </div>
+                              <div class="group" style="margin-top: 50px"> <input type="submit" name="login" class="button" value="Sign In"> </div>
+                          </form>
+                          <form id="register" class="sign-up-form" method="POST" action="">
+                              <div class="group"> <label for="f-name" class="label">First Name</label> <input id="f-name" name="f-name" type="text" class="input" placeholder="Type your Fist Name" required> </div>
+                              <div class="group"> <label for="l-name" class="label">Last Name</label> <input id="l-name" name="l-name" type="text" class="input" placeholder="Type your Last Name" required> </div>
+                              <div class="group"> <label id="register-password" for="pass-register" class="label">Password</label> <input id="pass-register" name="password" type="password" class="input" data-type="password" placeholder="Create your password" required> </div>
+                              <div class="group"> <label id="repeat-password" for="pass-confirm" class="label">Repeat Password</label> <input id="pass-confirm" type="password" class="input" data-type="password" placeholder="Repeat your password" required> </div>
+                              <div class="group"> <label for="email-register" class="label">Email Address</label> <input id="email-register" name="email" type="text" class="input" placeholder="Enter your email address" required> </div>
+                              <div class="group"> <input type="submit" name="register" class="button" value="Sign Up"> </div>
+                              <div class="hr"></div>
+                              <div class="foot"> <label for="tab-1">Already Member?</label> </div>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </div>
+
+    <script src="script.js"></script>
+  </body>
+
+</html>
---
+++ client/login-register/script.js
@@ -0,0 +1,72 @@
+window.addEventListener("load", () => {
+  if (document.getElementById("tab-2").checked) {
+    document.getElementById("login-box").style.minHeight = "800px";
+  }
+});
+
+const changeFormHeight = (param) => {
+  const form = document.getElementById("login-box");
+
+  switch (param) {
+    case "login":
+      form.style.minHeight = "450px";
+      break;
+
+    case "register":
+      form.style.minHeight = "800px";
+      break;
+
+    default:
+      break;
+  }
+};
+
+document
+  .getElementById("tab-1")
+  .addEventListener("click", () => changeFormHeight("login"));
+document
+  .getElementById("tab-2")
+  .addEventListener("click", () => changeFormHeight("register"));
+
+const checkPasswords = (event) => {
+  const password = document.getElementById("pass-register");
+  const confirmPassword = document.getElementById("pass-confirm");
+
+  const repeatLabel = document.getElementById("repeat-password");
+  const passwordLabel = document.getElementById("register-password");
+
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+
+  const reset = () => {
+    password.style.border = "none";
+    confirmPassword.style.border = "none";
+    repeatLabel.style.color = "#aaa";
+    repeatLabel.innerHTML = "REPEAT PASSWORD";
+    passwordLabel.style.color = "#aaa";
+    passwordLabel.innerHTML = "PASSWORD";
+  };
+
+  if (!passwordRegex.test(password.value)) {
+    event.preventDefault();
+
+    password.style.border = "1px solid #f00";
+    passwordLabel.style.color = "#f00";
+    passwordLabel.innerHTML =
+      "PASSWORD (your password must contain at least 8 characters, 1 number, 1 lowercase, 1 uppercase character and only numbers and letters!)";
+    password.addEventListener("change", reset);
+  } else if (password.value !== confirmPassword.value) {
+    event.preventDefault();
+    password.style.border = "1px solid #f00";
+    confirmPassword.style.border = "1px solid #f00";
+
+    repeatLabel.style.color = "#f00";
+    repeatLabel.innerHTML = "REPEAT PASSWORD (password does not match)";
+
+    password.addEventListener("change", reset);
+    confirmPassword.addEventListener("change", reset);
+  }
+};
+
+document
+  .getElementById("register")
+  .addEventListener("submit", (event) => checkPasswords(event));
